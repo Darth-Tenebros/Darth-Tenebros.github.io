14013e5e1ec8a40b63d423b12bccb678f62f46e7
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-02-24 19:35:58 +0200

    ugly darkmode - meh

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+function darkmode(){
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";  
+}
